4627345b093655401c2e7c6be8950ea9e12f4b76
Paulibyus <paulina.gonzalezx@gmail.com>
2023-10-02 13:58:59 -0300

    Seagregancards

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
-import { example } from './dataFunctions.js';
-import { renderItems } from './view.js';
+// import { type } from './dataFunctions.js';
+import { renderItems } from "./view.js";
 
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from "./data/pokemon/pokemon.js";
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, renderItems, data);
+const pokemonName = (pokemons) => {
+  const nombres = pokemons.map((pokemon) => {
+    return pokemon.name;
+  });
+  return nombres;
+};
+
+// console.log(example, renderItems, data);
+console.log(pokemonName(data.pokemon));
+
+const pokemons = data.pokemon;
+
+const pokemonsContainer = document.getElementById("pokemonsContainer");
+
+pokemonsContainer.innerHTML = "";
+pokemons.forEach((pokemon) => {
+  let pokemonCard = document.createElement("li");
+  pokemonCard.setAttribute("class", "contenedor-imagen-pokemon");
+  pokemonCard.innerHTML = `
+  <p class="pokename">${pokemon.name}</p>
+      <img class="img-pokemon" id="imagePokemon" src="${pokemon.img}"> 
+      `;
+  pokemonsContainer.appendChild(pokemonCard);
+});
